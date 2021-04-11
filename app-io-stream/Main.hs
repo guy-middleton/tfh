95e01301f8dbcd8c8203db75b4f818703cde210d
Guy Middleton <guy.middle@gmail.com>
2021-04-11 11:06:08 -0400

    Cleanup.

--- app-io-stream/Main.hs
+++ app-io-stream/Main.hs
@@ -6,21 +6,12 @@ module Main where
 
 import           Options.Applicative
 import           Data.Semigroup ((<>))
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import           Control.Monad
-import           System.IO.Streams (InputStream, OutputStream)
-import qualified System.IO.Streams as Streams
-import           Lib
-import System.IO
+import           System.IO
 import qualified Data.HashMap.Strict as HashMap
-import Streaming
-import Streaming.Prelude (yield, next, each, for, with, subst)
+import           Streaming
 import qualified Streaming.Prelude as S
 import qualified Streaming.ByteString.Char8 as Q
-import Control.Monad.Trans.Resource (runResourceT)
-import qualified Data.ByteString.Char8 as B
-
+import           Lib
 
 data Sample = Sample
     { filename :: String }
@@ -39,15 +30,12 @@ main = do
           <> progDesc "Read stuff from a file"
           <> header "tfh - read stats from web server logs"
     options <- execParser opts
+    out <- count (filename options)
+    print $ getCounts out
 
-    out <- withFile (filename options) ReadMode $ \h -> do
-        -- Q.stdout
-        -- $ Q.unlines
-        -- $ subst Q.chunk
-        S.fold_ buildMap'' HashMap.empty id
-        $ mapped Q.toStrict
-        $ Q.lines        -- Stream (ByteString m) m () -- divided into Stream layers
-        $ Q.fromHandle h -- ByteString m ()            -- raw bytes
-
-    print $ getCounts'' out
-    -- print $ getCounts' counts
+count :: FilePath -> IO (HashMap.HashMap BKey Int)
+count filename = withFile filename ReadMode $ \h -> do
+    S.fold_ buildMap HashMap.empty id
+    $ mapped Q.toStrict
+    $ Q.lines
+    $ Q.fromHandle h
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,107 +2,38 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 module Lib
-    ( getKeys
-    , getCountsMap
-    , getCounts
-    , getCounts'
-    , getCounts''
-    , getTop
-    , cat
+    ( getCounts
     , buildMap
-    , buildMap'
-    , buildMap''
-    , Key (..)
     , BKey (..)
     ) where
 
-import Safe
-import Data.List
-import Data.Maybe
-import Data.Ord
-import qualified Data.Text as T
-import Data.Text.Encoding         as T
--- import qualified Data.Text.ICU as ICU
+import           Safe
+import           Data.List
+import           Data.Maybe
+import           Data.Ord
+import           Data.Char
 import qualified Data.HashMap.Strict as HashMap
 import qualified Data.ByteString.Char8 as B
-import Data.Char
-
-
-
-import GHC.Generics (Generic)
-import Data.Hashable
-
-data Key = Key { key :: !T.Text }
-    deriving (Show, Eq, Generic)
-
-instance Hashable Key
+import           GHC.Generics (Generic)
+import           Data.Hashable
 
 data BKey = BKey { bkey :: !B.ByteString }
     deriving (Show, Eq, Generic)
 
 instance Hashable BKey
 
-getKeys :: T.Text -> [Maybe T.Text]
-getKeys ws = map (gf 0 . T.words) $ T.lines ws
-    where gf n l = atMay l n
-
-getKeys' :: T.Text -> [Maybe Key]
-getKeys' ws = map (gf 0 . T.words) $ T.lines ws
-    where gf n l = Key <$> atMay l n
-
-getCountsMap :: [Maybe T.Text] -> HashMap.HashMap T.Text Int
-getCountsMap xs = HashMap.fromListWith (+) [(x, 1 :: Int) | x <- catMaybes xs]
-
-getCountsMap' :: [Maybe Key] -> HashMap.HashMap Key Int
-getCountsMap' xs = HashMap.fromListWith (+) [(x, 1 :: Int) | x <- catMaybes xs]
-
-getCounts :: HashMap.HashMap T.Text Int -> [(T.Text, Int)]
+getCounts :: HashMap.HashMap BKey Int -> [(BKey, Int)]
 getCounts xs = take 10 $ sortOn (Down . snd) $ HashMap.toList xs
 
-getCounts' :: HashMap.HashMap Key Int -> [(Key, Int)]
-getCounts' xs = take 10 $ sortOn (Down . snd) $ HashMap.toList xs
-
-getCounts'' :: HashMap.HashMap BKey Int -> [(BKey, Int)]
-getCounts'' xs = take 10 $ sortOn (Down . snd) $ HashMap.toList xs
-
-getTop :: T.Text -> [(T.Text, Int)]
-getTop ws = getCounts $ getCountsMap $ getKeys ws
-
-getTop' :: T.Text -> [(Key, Int)]
-getTop' ws = getCounts' $ getCountsMap' $ getKeys' ws
-
-getKeyFromLine :: T.Text -> Maybe T.Text
-getKeyFromLine l = gf 0 . T.words $ l
-    where gf n l = atMay l n
-
-getKeyFromLine' :: T.Text -> Maybe Key
-getKeyFromLine' l = gf 0 . T.words $ l
-    where gf n l = Key <$> atMay l n
-
-getKeyFromLine'' :: B.ByteString  -> Maybe BKey
-getKeyFromLine'' l = gf 0 . B.words $ l
+getKeyFromLine :: B.ByteString  -> Maybe BKey
+getKeyFromLine l = gf 0 . B.words $ l
     where gf n l = BKey <$> B.copy <$> atMay l n
 
-getKeyFromLine''' :: B.ByteString  -> Maybe BKey
-getKeyFromLine''' l = gf 0 l
+getKeyFromLine' :: B.ByteString  -> Maybe BKey
+getKeyFromLine' l = gf 0 l
     where gf n l = Just $ BKey $ B.copy $ B.takeWhile (not . isSpace) l
 
-cat :: T.Text -> T.Text -> T.Text
-cat l r = case getKeyFromLine r of
-    Nothing -> l
-    (Just s) -> T.append l $ T.append " " s
-
-buildMap :: HashMap.HashMap T.Text Int -> T.Text -> HashMap.HashMap T.Text Int
+buildMap :: HashMap.HashMap BKey Int -> B.ByteString  -> HashMap.HashMap BKey Int
 buildMap m t = case getKeyFromLine t of
-    Nothing -> m
-    (Just k) -> HashMap.insertWith (+) k 1 m
-
-buildMap' :: HashMap.HashMap Key Int -> T.Text -> HashMap.HashMap Key Int
-buildMap' m t = case getKeyFromLine' t of
-    Nothing -> m
-    (Just k) -> HashMap.insertWith (+) k 1 m
-
-buildMap'' :: HashMap.HashMap BKey Int -> B.ByteString  -> HashMap.HashMap BKey Int
-buildMap'' m t = case getKeyFromLine''' t of
     Nothing -> m
     (Just k) -> HashMap.insertWith (+) k 1 m
