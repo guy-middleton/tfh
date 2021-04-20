b931fbd551289afb5648897f27ab93b784dd4e51
Guy Middleton <guy.middle@gmail.com>
2021-04-19 22:07:27 -0400

    Use ShortByteString in BKey.

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,22 +12,24 @@ import           Data.Ord
 import           Data.Char
 import qualified Data.HashMap.Strict as HashMap
 import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Short as B
+         (ShortByteString, toShort, fromShort)
 import           GHC.Generics (Generic)
 import           Data.Hashable
 
-data BKey = BKey { bkey :: !B.ByteString }
+data BKey = BKey { bkey :: !B.ShortByteString }
     deriving (Eq, Generic)
 
 instance Hashable BKey
 instance Show BKey where
-    show = B.unpack . bkey
+    show = B.unpack . B.fromShort . bkey
 
 getCounts :: HashMap.HashMap BKey Int -> Int -> [(BKey, Int)]
 getCounts xs n = take n $ sortOn (Down . snd) $ HashMap.toList xs
 
 getKeyFromLine :: Int -> B.ByteString  -> Maybe BKey
 getKeyFromLine n l = gf n . B.words $ l
-    where gf n l = BKey <$> B.copy <$> atMay l n
+    where gf n l = BKey <$> B.toShort <$> atMay l n
 
 buildMap :: Int -> HashMap.HashMap BKey Int -> B.ByteString  -> HashMap.HashMap BKey Int
 buildMap n m t = case getKeyFromLine n t of
